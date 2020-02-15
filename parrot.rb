2575fed4f3d82645538a93c1ccb9af41cf4bc963
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-15 20:13:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(name ="Squawk")
-  puts name
-end 
-return name 
-end 
+# def parrot(name ="Squawk")
+#   puts name
+# end 
+# return name 
+# end 
   
